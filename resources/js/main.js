4db4bdbf2bc5f85902ce349bd6cad8b4700c4814
Pedro Liska <pedro.liska@extendhealth.com>
2014-03-18 09:16:28 -0600

    you can now add items

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,6 +7,21 @@ todoApp.controller('TodoCtrl', function($scope){
     'buy new bike tube',
     'tune your bike',
     'change bike pedals'].map(function (item) {
-      return {'description': item, done: false};
+      return descToTodo(item);
     });
+  $scope.todos[3].done = true;
+  $scope.addTodo = function(description) {
+    $scope.todos.push(descToTodo(description));
+  };
+  $scope.deleteDone = function() {
+    var indecesToDelete = $scope.todos
+      .map(function(item, index){ 
+        return {'index': index, 'done':item.done}; 
+      })
+      .filter(function (item){ return done; });
+  };
+
+  function descToTodo(desc) {
+    return {'description': desc, done: false};
+  }
 });
